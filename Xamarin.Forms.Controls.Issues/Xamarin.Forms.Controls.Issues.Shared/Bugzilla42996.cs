bfb338f8ec95163c69367480d587abb0421896ea
Samantha Houts <samantha.houts@xamarin.com>
2017-02-27 14:08:05 -0800

    Add repro for 42996

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla42996.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System.Linq;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 42996, "CarouselView does not layout children properly on UWP", PlatformAffected.UWP)]
+	public class Bugzilla42996 : TestNavigationPage
+	{
+		protected override void Init()
+		{
+			var page = new ContentPage();
+			var carouselView = new CarouselView
+			{
+				ItemsSource = Enumerable.Range(0, 10).ToList(),
+				ItemTemplate = new DataTemplate(() =>
+				{
+					var label = new Label { Text = "Blah", BackgroundColor = Color.White, Margin = new Thickness(10) };
+					var stack = new StackLayout { Children = { label } };
+					return new ScrollView { Content = stack };
+				})
+			};
+
+			page.Content = carouselView;
+
+			Navigation.PushAsync(page);
+		}
+	}
+}
