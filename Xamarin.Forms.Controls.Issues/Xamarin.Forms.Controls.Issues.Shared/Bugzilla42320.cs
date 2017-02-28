6568fa8f9f359decd736e0a7f80120c7e54addc4
Samantha Houts <samantha.houts@xamarin.com>
2017-02-28 09:11:05 -0800

    Add repro for 42320

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla42320.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 42320, "Full page CarouselView with embedded Listview is hardly usable", PlatformAffected.Android)]
+	public class Bugzilla42320 : TestContentPage
+	{
+		protected override void Init()
+		{
+			Content = new CarouselView
+			{
+				ItemsSource = Enumerable.Range(0, 10).ToList(),
+				ItemTemplate = new DataTemplate(() =>
+				{
+					return new ListView
+					{
+						ItemsSource = Enumerable.Range(0, 100),
+						ItemTemplate = new DataTemplate(() =>
+						{
+							return new ViewCell()
+							{
+								View = new Label { Text = "Scroll the ListView normally. If the CarouselView swipes easily or the ListView fails to scroll properly, this test has failed." }
+							};
+						})
+					};
+				})
+			};
+		}
+	}
+}
