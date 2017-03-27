150a3ba26d66b1bfd1665e6f084c83ca0e8e3a61
Samantha Houts <samantha.houts@xamarin.com>
2017-03-27 10:43:44 -0700

    Add repro for 42230

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla42230.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System.Linq;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 42230, "CarouselView Moves to First item on Rotation (iOS)", PlatformAffected.iOS)]
+	public class Bugzilla42230 : TestContentPage
+	{
+		protected override void Init()
+		{
+			var cv = new CarouselView
+			{
+				ItemsSource = Enumerable.Range(0, 10).ToList(),
+				ItemTemplate = new DataTemplate(() =>
+				{
+					var label = new Label();
+					label.SetBinding(Label.TextProperty, ".");
+					return label;
+				})
+			};
+
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "The CarouselView should display \"1\". Rotate the device. If the CarouselView does not still display \"1\", this test has failed." },
+					cv
+				}
+			};
+
+			cv.Position = 1;
+		}
+	}
+}
