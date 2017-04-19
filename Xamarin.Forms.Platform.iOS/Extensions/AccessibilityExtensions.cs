15987eef1e0db59d1aef139739b1e30c22ce0991
Samantha Houts <samantha.houts@xamarin.com>
2017-04-19 14:30:31 -0700

    [iOS] Set A11y properties on Toolbaritems

--- Xamarin.Forms.Platform.iOS/Extensions/AccessibilityExtensions.cs
+++ Xamarin.Forms.Platform.iOS/Extensions/AccessibilityExtensions.cs
@@ -1,10 +1,10 @@
-﻿using NativeView = UIKit.UIView;
+﻿using UIKit;
 
 namespace Xamarin.Forms.Platform.iOS
 {
 	public static class AccessibilityExtensions
 	{
-		public static string SetAccessibilityHint(this NativeView Control, Element Element, string _defaultAccessibilityHint = null)
+		public static string SetAccessibilityHint(this UIView Control, Element Element, string _defaultAccessibilityHint = null)
 		{
 			if (Element == null)
 				return _defaultAccessibilityHint;
@@ -17,7 +17,7 @@ namespace Xamarin.Forms.Platform.iOS
 			return _defaultAccessibilityHint;
 		}
 
-		public static string SetAccessibilityLabel(this NativeView Control, Element Element, string _defaultAccessibilityLabel = null)
+		public static string SetAccessibilityLabel(this UIView Control, Element Element, string _defaultAccessibilityLabel = null)
 		{
 			if (Element == null)
 				return _defaultAccessibilityLabel;
@@ -30,7 +30,33 @@ namespace Xamarin.Forms.Platform.iOS
 			return _defaultAccessibilityLabel;
 		}
 
-		public static bool? SetIsAccessibilityElement(this NativeView Control, Element Element, bool? _defaultIsAccessibilityElement = null)
+		public static string SetAccessibilityHint(this UIBarItem Control, Element Element, string _defaultAccessibilityHint = null)
+		{
+			if (Element == null)
+				return _defaultAccessibilityHint;
+
+			if (_defaultAccessibilityHint == null)
+				_defaultAccessibilityHint = Control.AccessibilityHint;
+
+			Control.AccessibilityHint = (string)Element.GetValue(Accessibility.HintProperty) ?? _defaultAccessibilityHint;
+
+			return _defaultAccessibilityHint;
+		}
+
+		public static string SetAccessibilityLabel(this UIBarItem Control, Element Element, string _defaultAccessibilityLabel = null)
+		{
+			if (Element == null)
+				return _defaultAccessibilityLabel;
+
+			if (_defaultAccessibilityLabel == null)
+				_defaultAccessibilityLabel = Control.AccessibilityLabel;
+
+			Control.AccessibilityLabel = (string)Element.GetValue(Accessibility.NameProperty) ?? _defaultAccessibilityLabel;
+
+			return _defaultAccessibilityLabel;
+		}
+
+		public static bool? SetIsAccessibilityElement(this UIView Control, Element Element, bool? _defaultIsAccessibilityElement = null)
 		{
 			if (Element == null)
 				return _defaultIsAccessibilityElement;
