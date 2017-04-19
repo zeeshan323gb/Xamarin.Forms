db5cd5273250ebc3f5c0e47f578182964bd079d2
Rui Marinho <me@ruimarinho.net>
2017-04-19 16:09:54 +0100

    [Flexlayout] Add more Flexlayout margin tests

--- Xamarin.Forms.FlexLayout/Xamarin.Forms.FlexLayout.Tests/FlexLayoutMarginTests.cs
+++ Xamarin.Forms.FlexLayout/Xamarin.Forms.FlexLayout.Tests/FlexLayoutMarginTests.cs
@@ -221,13 +221,13 @@ namespace Xamarin.Forms.FlexLayoutTests
 
 			Assert.AreEqual(0f, layout.X);
 			Assert.AreEqual(0f, layout.Y);
-			Assert.AreEqual(100f, layout.Width);
-			Assert.AreEqual(100f, layout.Height);
+			Assert.AreEqual(100, layout.Width);
+			Assert.AreEqual(100, layout.Height);
 
-			Assert.AreEqual(10f, view1.X);
-			Assert.AreEqual(0f, view1.Y);
-			Assert.AreEqual(80f, view1.Width);
-			Assert.AreEqual(100f, view1.Height);
+			Assert.AreEqual(10, view1.X);
+			Assert.AreEqual(0, view1.Y);
+			Assert.AreEqual(80, view1.Width);
+			Assert.AreEqual(100, view1.Height);
 		}
 
 		[Test]
@@ -250,21 +250,58 @@ namespace Xamarin.Forms.FlexLayoutTests
 			var measure = layout.Measure(100, 100);
 			layout.Layout(new Rectangle(0, 0, 100, 100));
 
+			Assert.AreEqual(0, layout.X);
+			Assert.AreEqual(0, layout.Y);
+			Assert.AreEqual(100, layout.Width);
+			Assert.AreEqual(100, layout.Height);
+
+			Assert.AreEqual(0, view1.X);
+			Assert.AreEqual(0, view1.Y);
+			Assert.AreEqual(45, view1.Width);
+			Assert.AreEqual(100, view1.Height);
+
+			Assert.AreEqual(55, view2.X);
+			Assert.AreEqual(0, view2.Y);
+			Assert.AreEqual(45, view2.Width);
+			Assert.AreEqual(100, view2.Height);
+		}
+
+		[Test]
+		public void TestMarginWithSiblingColumn()
+		{
+			var platform = new UnitPlatform();
+			var layout = new FlexLayout();
+			layout.FlexDirection = Flex.FlexDirection.Column;
+			layout.WidthRequest = 100;
+			layout.HeightRequest = 100;
+			layout.Platform = platform;
+
+			var view1 = new View { Platform = platform, IsPlatformEnabled = true };
+			view1.Margin = new Thickness(0, 0, 0, 10);
+			FlexLayout.SetGrow(view1, 1);
+			layout.Children.Add(view1);
+
+			var view2 = new View { Platform = platform, IsPlatformEnabled = true };
+			FlexLayout.SetGrow(view2, 1);
+			layout.Children.Add(view2);
+
+			var measure = layout.Measure(100, 100);
+			layout.Layout(new Rectangle(0, 0, 100, 100));
+
 			Assert.AreEqual(0f, layout.X);
 			Assert.AreEqual(0f, layout.Y);
 			Assert.AreEqual(100f, layout.Width);
 			Assert.AreEqual(100f, layout.Height);
 
-			Assert.AreEqual(0f, view1.X);
-			Assert.AreEqual(0f, view1.Y);
-			Assert.AreEqual(45f, view1.Width);
-			Assert.AreEqual(100f, view1.Height);
+			Assert.AreEqual(0, view1.X);
+			Assert.AreEqual(0, view1.Y);
+			Assert.AreEqual(100, view1.Width);
+			Assert.AreEqual(45, view1.Height);
 
-			Assert.AreEqual(55f, view2.X);
-			Assert.AreEqual(0f, view2.Y);
-			Assert.AreEqual(45f, view2.Width);
-			Assert.AreEqual(100f, view2.Height);
+			Assert.AreEqual(0, view2.X);
+			Assert.AreEqual(55, view2.Y);
+			Assert.AreEqual(100, view2.Width);
+			Assert.AreEqual(45, view2.Height);
 		}
-
 	}
 }
