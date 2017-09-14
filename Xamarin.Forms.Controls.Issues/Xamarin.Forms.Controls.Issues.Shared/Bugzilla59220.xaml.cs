a138e5ffe74bcd0282fa2647e26bc896cc3a43c8
Stephane Delcroix <stephane@delcroix.org>
2017-09-14 14:15:56 +0200

    test for 59220

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla59220.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if APP
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 59220, "Dot Binding Syntax", PlatformAffected.All)]
+	public partial class Bugzilla59220 : ContentPage, IValueConverter
+	{
+		public Bugzilla59220()
+		{
+			InitializeComponent();
+			BindingContext = new {
+				Source = new List<ListItem> {
+					new ListItem {Name="Foo"},
+					new ListItem {Name="Bar"},
+					new ListItem {Name="Foo"},
+					new ListItem {Name="Bar"},
+					new ListItem {Name="Foo"},
+					new ListItem {Name="Bar"},
+					new ListItem {Name="Foo"},
+					new ListItem {Name="Bar"},
+					new ListItem {Name="Foo"},
+					new ListItem {Name="Bar"},
+					new ListItem {Name="Foo"},
+					new ListItem {Name="Bar"},
+				}
+			};
+		}
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (value as ListItem)?.Name;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public class ListItem
+		{
+			public string Name { get; set; }
+		}
+	}
+#endif
+}
