a38bb2c76562d0c6bebfe9d92a1244c32e8ad903
Chris King <kingces95@gmail.com>
2017-10-10 11:27:28 -0400

    UITest for ListView caching

---
+++ runxtc.bat
@@ -0,0 +1,10 @@
+packages\Xamarin.UITest.2.1.2\tools\test-cloud.exe submit ^
+Xamarin.Forms.ControlGallery.Android\bin\Debug\AndroidControlGallery.AndroidControlGallery-Signed.apk ^
+13d622b3fbb487d8f5d0791278f0908d ^
+--include kingc ^
+--devices 4de66df4 ^
+--series "master" ^
+--locale "en_US" ^
+--app-name "AndroidControlGallery" ^
+--user kingces95@gmail.com ^
+--assembly-dir Xamarin.Forms.Core.Android.UITests\bin\Debug\
