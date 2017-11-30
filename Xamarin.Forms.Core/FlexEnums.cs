a9469cfe97ca9edfd59bb9612828e5059292df18
Stephane Delcroix <stephane@delcroix.org>
2017-11-30 08:58:50 +0100

    [*] Flex Layout
    Add a FlexLayout. The implementation is a wrapper around Xamarin.Flex.

---
+++ Xamarin.Forms.Core/FlexEnums.cs
@@ -0,0 +1,91 @@
+﻿using System;
+namespace Xamarin.Forms
+{
+	//TODO require a typeConverter for css values
+	public enum FlexJustify
+	{
+		FlexStart = Flex.Align.Start,
+		Center = Flex.Align.Center,
+		FlexEnd = Flex.Align.End,
+		SpaceBetween = Flex.Align.SpaceBetween,
+		SpaceAround = Flex.Align.SpaceAround,
+		SpaceEvenly = Flex.Align.SpaceEvenly,
+	}
+
+	public enum FlexPosition
+	{
+		Relative = Flex.Position.Relative,
+		Absolute = Flex.Position.Absolute,
+	}
+
+	//TODO require a typeConverter for css values
+	public enum FlexDirection
+	{
+		Column = Flex.Direction.Column,
+		ColumnReverse = Flex.Direction.ColumnReverse,
+		Row = Flex.Direction.Row,
+		RowReverse = Flex.Direction.RowReverse,
+	}
+
+	//TODO require a typeConverter for css values
+	public enum FlexAlignContent
+	{
+		Stretch = Flex.Align.Stretch,
+		Center = Flex.Align.Center,
+		FlexStart = Flex.Align.Start,
+		FlexEnd = Flex.Align.End,
+		SpaceBetween = Flex.Align.SpaceBetween,
+		SpaceAround = Flex.Align.SpaceAround,
+	}
+
+	//TODO require a typeConverter for css values
+	public enum FlexAlignItems
+	{
+		Stretch = Flex.Align.Stretch,
+		Center = Flex.Align.Center,
+		FlexStart = Flex.Align.Start,
+		FlexEnd = Flex.Align.End,
+		//Baseline = Flex.Align.Baseline,
+	}
+
+	//TODO require a typeConverter for css values
+	public enum FlexAlignSelf
+	{
+		Auto = Flex.Align.Auto,
+		Stretch = Flex.Align.Stretch,
+		Center = Flex.Align.Center,
+		FlexStart = Flex.Align.Start,
+		FlexEnd = Flex.Align.End,
+		//Baseline = Flex.Align.Baseline,
+	}
+
+	//TODO require a typeConverter for css values
+	public enum FlexWrap
+	{
+		NoWrap = Flex.Wrap.NoWrap,
+		Wrap = Flex.Wrap.Wrap,
+		Reverse = Flex.Wrap.WrapReverse,
+	}
+
+	//TODO require a typeConverter
+	public struct FlexBasis
+	{
+		bool _isLength;
+		public static FlexBasis Auto = new FlexBasis();
+		public float Length { get; }
+		internal bool IsAuto => !_isLength;
+
+		public FlexBasis(float length)
+		{
+			if (length < 0)
+				throw new ArgumentException("should be a positive value", nameof(length));
+			_isLength = true;
+			Length = length;
+		}
+
+		public static implicit operator FlexBasis(float length)
+		{
+			return new FlexBasis(length);
+		}
+	}
+}
---
+++ Xamarin.Forms.Core/FlexLayout.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms
+{
+	public class FlexLayout : Layout<View>
+	{
+		public static readonly BindableProperty DirectionProperty =
+			BindableProperty.Create(nameof(Direction), typeof(FlexDirection), typeof(FlexLayout), FlexDirection.Row,
+									propertyChanged: OnDirectionPropertyChanged);
+
+		public static readonly BindableProperty JustifyContentProperty =
+			BindableProperty.Create(nameof(JustifyContent), typeof(FlexJustify), typeof(FlexLayout), FlexJustify.FlexStart,
+									propertyChanged: OnJustifyContentPropertyChanged);
+
+		public static readonly BindableProperty AlignContentProperty =
+			BindableProperty.Create(nameof(AlignContent), typeof(FlexAlignContent), typeof(FlexLayout), FlexAlignContent.Stretch,
+									propertyChanged: OnAlignContentPropertyChanged);
+
+		public static readonly BindableProperty AlignItemsProperty =
+			BindableProperty.Create(nameof(AlignItems), typeof(FlexAlignItems), typeof(FlexLayout), FlexAlignItems.Stretch,
+									propertyChanged: OnAlignItemsPropertyChanged);
+
+		public static readonly BindableProperty PositionProperty =
+			BindableProperty.Create(nameof(Position), typeof(FlexPosition), typeof(FlexLayout), FlexPosition.Relative,
+									propertyChanged: OnPositionPropertyChanged);
+
+		public static readonly BindableProperty WrapProperty =
+			BindableProperty.Create(nameof(Wrap), typeof(FlexWrap), typeof(FlexLayout), FlexWrap.NoWrap,
+									propertyChanged: OnWrapPropertyChanged);
+
+		static readonly BindableProperty FlexItemProperty =
+			BindableProperty.CreateAttached("FlexItem", typeof(Flex.Item), typeof(FlexLayout), null);
+
+		public static readonly BindableProperty OrderProperty =
+			BindableProperty.CreateAttached("Order", typeof(int), typeof(FlexLayout), default(int),
+											propertyChanged: OnOrderPropertyChanged);
+
+		public static readonly BindableProperty GrowProperty =
+			BindableProperty.CreateAttached("Grow", typeof(float), typeof(FlexLayout), default(float),
+											propertyChanged: OnGrowPropertyChanged);
+
+		public static readonly BindableProperty ShrinkProperty =
+			BindableProperty.CreateAttached("Shrink", typeof(float), typeof(FlexLayout), 1f,
+											propertyChanged: OnShrinkPropertyChanged);
+
+		public static readonly BindableProperty AlignSelfProperty =
+			BindableProperty.CreateAttached("AlignSelf", typeof(FlexAlignSelf), typeof(FlexLayout), FlexAlignSelf.Auto,
+											propertyChanged: OnAlignSelfPropertyChanged);
+
+		public static readonly BindableProperty BasisProperty =
+			BindableProperty.CreateAttached("Basis", typeof(FlexBasis), typeof(FlexLayout), FlexBasis.Auto,
+											propertyChanged: OnBasisPropertyChanged);
+
+		public FlexDirection Direction {
+			get => (FlexDirection)GetValue(DirectionProperty);
+			set => SetValue(DirectionProperty, value);
+		}
+
+		public FlexJustify JustifyContent {
+			get => (FlexJustify)GetValue(JustifyContentProperty);
+			set => SetValue(JustifyContentProperty, value);
+		}
+
+		public FlexAlignContent AlignContent {
+			get => (FlexAlignContent)GetValue(AlignContentProperty);
+			set => SetValue(AlignContentProperty, value);
+		}
+
+		public FlexAlignItems AlignItems {
+			get => (FlexAlignItems)GetValue(AlignItemsProperty);
+			set => SetValue(AlignItemsProperty, value);
+		}
+
+		public FlexPosition Position {
+			get => (FlexPosition)GetValue(PositionProperty);
+			set => SetValue(PositionProperty, value);
+		}
+
+		public FlexWrap Wrap {
+			get => (FlexWrap)GetValue(WrapProperty);
+			set => SetValue(WrapProperty, value);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		static Flex.Item GetFlexItem(BindableObject bindable)
+			=> (Flex.Item)bindable.GetValue(FlexItemProperty);
+
+		static void SetFlexItem(BindableObject bindable, Flex.Item node)
+			=> bindable.SetValue(FlexItemProperty, node);
+
+		public static int GetOrder(BindableObject bindable)
+			=> (int)bindable.GetValue(OrderProperty);
+
+		public static void SetOrder(BindableObject bindable, int value)
+			=> bindable.SetValue(OrderProperty, value);
+
+		public static float GetGrow(BindableObject bindable)
+			=> (float)bindable.GetValue(GrowProperty);
+
+		public static void SetGrow(BindableObject bindable, float value)
+			=> bindable.SetValue(GrowProperty, value);
+
+		public static float GetShrink(BindableObject bindable)
+			=> (float)bindable.GetValue(ShrinkProperty);
+
+		public static void SetShrink(BindableObject bindable, float value)
+			=> bindable.SetValue(ShrinkProperty, value);
+
+		public static FlexAlignSelf GetAlignSelf(BindableObject bindable)
+			=> (FlexAlignSelf)bindable.GetValue(AlignSelfProperty);
+
+		public static void SetAlignSelf(BindableObject bindable, FlexAlignSelf value)
+			=> bindable.SetValue(AlignSelfProperty, value);
+
+		public static FlexBasis GetBasis(BindableObject bindable)
+			=> (FlexBasis)bindable.GetValue(BasisProperty);
+
+		public static void SetBasis(BindableObject bindable, FlexBasis value)
+			=> bindable.SetValue(BasisProperty, value);
+
+		static void OnOrderPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			if (bindable.GetIsDefault(FlexItemProperty))
+				return;
+			GetFlexItem(bindable).Order = (int)newValue;
+		}
+
+		static void OnGrowPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			if (bindable.GetIsDefault(FlexItemProperty))
+				return;
+			GetFlexItem(bindable).Grow = (float)newValue;
+		}
+
+		static void OnShrinkPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			if (bindable.GetIsDefault(FlexItemProperty))
+				return;
+			GetFlexItem(bindable).Shrink = (float)newValue;
+		}
+
+		static void OnAlignSelfPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			if (bindable.GetIsDefault(FlexItemProperty))
+				return;
+			GetFlexItem(bindable).AlignSelf = (Flex.Align)(FlexAlignSelf)newValue;
+		}
+
+		static void OnBasisPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			if (bindable.GetIsDefault(FlexItemProperty))
+				return;
+			GetFlexItem(bindable).Basis = ((FlexBasis)newValue).ToFlexFloat();
+		}
+
+		static void OnDirectionPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var flexLayout = bindable as FlexLayout;
+			if (flexLayout._root == null)
+				return;
+			flexLayout._root.Direction = (Flex.Direction)(FlexDirection)newValue;
+			flexLayout.InvalidateLayout();
+		}
+
+		static void OnJustifyContentPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var flexLayout = bindable as FlexLayout;
+			if (flexLayout._root == null)
+				return;
+			flexLayout._root.JustifyContent = (Flex.Align)(FlexJustify)newValue;
+			flexLayout.InvalidateLayout();
+		}
+
+		static void OnAlignContentPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var flexLayout = bindable as FlexLayout;
+			if (flexLayout._root == null)
+				return;
+			flexLayout._root.AlignContent = (Flex.Align)(FlexAlignContent)newValue;
+			flexLayout.InvalidateLayout();
+		}
+
+		static void OnAlignItemsPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var flexLayout = (FlexLayout)bindable;
+			if (flexLayout._root == null)
+				return;
+			flexLayout._root.AlignItems = (Flex.Align)(FlexAlignItems)newValue;
+			flexLayout.InvalidateLayout();
+		}
+
+		static void OnPositionPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var flexLayout = (FlexLayout)bindable;
+			if (flexLayout._root == null)
+				return;
+			flexLayout._root.Position = (Flex.Position)(FlexPosition)newValue;
+			flexLayout.InvalidateLayout();
+		}
+
+		static void OnWrapPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var flexLayout = bindable as FlexLayout;
+			if (flexLayout._root == null)
+				return;
+			flexLayout._root.Wrap = (Flex.Wrap)(FlexWrap)newValue;
+			flexLayout.InvalidateLayout();
+		}
+
+		Flex.Item _root;
+		//this should only be used in unitTests. layout creation will normally happen on OnParentSet
+		internal override void OnIsPlatformEnabledChanged()
+		{
+			base.OnIsPlatformEnabledChanged();
+			if (IsPlatformEnabled && _root == null)
+				PopulateLayout();
+			else if(!IsPlatformEnabled && _root != null)
+				ClearLayout();
+		}
+
+		protected override void OnParentSet()
+		{
+			base.OnParentSet();
+			if (Parent != null && _root == null)
+				PopulateLayout();
+			else if (Parent == null && _root != null)
+				ClearLayout();
+		}
+
+		void PopulateLayout()
+		{
+			InitLayoutProperties(_root = new Flex.Item());
+			foreach (var child in Children)
+				AddChild(child);
+		}
+
+		void InitLayoutProperties(Flex.Item item)
+		{
+			var values = GetValues(AlignContentProperty,
+								   AlignItemsProperty,
+								   DirectionProperty,
+								   JustifyContentProperty,
+								   WrapProperty);
+			item.AlignContent = (Flex.Align)(FlexAlignContent)values[0];
+			item.AlignItems = (Flex.Align)(FlexAlignItems)values[1];
+			item.Direction = (Flex.Direction)(FlexDirection)values[2];
+			item.JustifyContent = (Flex.Align)(FlexJustify)values[3];
+			item.Wrap = (Flex.Wrap)(FlexWrap)values[4];
+		}
+
+		void ClearLayout()
+		{
+			foreach (var child in Children)
+				RemoveChild(child);
+			_root.Dispose();
+			_root = null;
+		}
+
+		protected override void OnAdded(View view)
+		{
+			AddChild(view);
+			view.PropertyChanged += OnChildPropertyChanged;
+			base.OnAdded(view);
+		}
+
+		protected override void OnRemoved(View view)
+		{
+			view.PropertyChanged -= OnChildPropertyChanged;
+			RemoveChild(view);
+			base.OnRemoved(view);
+		}
+
+		void AddChild(View view)
+		{
+			if (_root == null)
+				return;
+			var item = (view as FlexLayout)?._root ?? new Flex.Item();
+			InitItemProperties(view, item);
+			if (!(view is FlexLayout)) { //inner layouts don't get measured
+				item.SelfSizing = (Flex.Item it, ref float w, ref float h) => {
+					(var widthConstraint, var heightConstraint) = item.GetConstraints();
+					var request = view.Measure(widthConstraint, heightConstraint).Request;
+					w = (float)request.Width;
+					h = (float)request.Height;
+				};
+			}
+
+			_root.InsertAt(Children.IndexOf(view), item);
+			SetFlexItem(view, item);
+		}
+
+		void InitItemProperties(View view, Flex.Item item)
+		{
+			var values = view.GetValues(OrderProperty,
+										GrowProperty,
+										ShrinkProperty,
+										BasisProperty,
+										AlignSelfProperty,
+										MarginProperty,
+										WidthRequestProperty,
+										HeightRequestProperty);
+			item.Order = (int)values[0];
+			item.Grow = (float)values[1];
+			item.Shrink = (float)values[2];
+			item.Basis = ((FlexBasis)values[3]).ToFlexFloat();
+			item.AlignSelf = (Flex.Align)(FlexAlignSelf)values[4];
+			item.MarginLeft = (float)((Thickness)values[5]).Left;
+			item.MarginTop = (float)((Thickness)values[5]).Top;
+			item.MarginRight = (float)((Thickness)values[5]).Right;
+			item.MarginBottom = (float)((Thickness)values[5]).Bottom;
+			item.Width = (double)values[6] < 0 ? float.NaN : (float)(double)values[6];
+			item.Height = (double)values[7] < 0 ? float.NaN : (float)(double)values[7];
+		}
+
+		void RemoveChild(View view)
+		{
+			if (_root == null)
+				return;
+			var item = GetFlexItem(view);
+			_root.Remove(item);
+			item.Dispose();
+			view.ClearValue(FlexItemProperty);
+		}
+
+		void OnChildPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (   e.PropertyName == WidthRequestProperty.PropertyName
+				|| e.PropertyName == HeightRequestProperty.PropertyName) {
+				var item = (sender as FlexLayout)?._root ?? GetFlexItem((BindableObject)sender);
+				item.Width = ((View)sender).WidthRequest < 0 ? float.NaN : (float)((View)sender).WidthRequest;
+				item.Height = ((View)sender).HeightRequest < 0 ? float.NaN : (float)((View)sender).HeightRequest;
+				InvalidateMeasureInternal(InvalidationTrigger.MeasureChanged);
+				UpdateChildrenLayout();
+			}
+
+			if (   e.PropertyName == OrderProperty.PropertyName
+				|| e.PropertyName == GrowProperty.PropertyName
+				|| e.PropertyName == ShrinkProperty.PropertyName
+				|| e.PropertyName == BasisProperty.PropertyName
+				|| e.PropertyName == AlignSelfProperty.PropertyName
+			    || e.PropertyName == MarginProperty.PropertyName) {
+				InvalidateMeasureInternal(InvalidationTrigger.MeasureChanged);
+				UpdateChildrenLayout();
+			}
+		}
+
+		protected override void LayoutChildren(double x, double y, double width, double height)
+		{
+			Layout(x, y, width, height);
+			foreach (var child in Children)
+				child.Layout(GetFlexItem(child).GetFrame());
+		}
+
+		protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+		{
+			//All of this is a HACK as X.Flex doesn't supports measuring
+			if (!double.IsPositiveInfinity(widthConstraint) && !double.IsPositiveInfinity(heightConstraint))
+				return new SizeRequest(new Size(widthConstraint, heightConstraint));
+
+			//1. Set Shrink to 0;
+			foreach (var item in _root)
+				item.Shrink = 0;
+
+			Layout(0, 0, widthConstraint, heightConstraint);
+
+			//2. look at the children location
+			if (double.IsPositiveInfinity(widthConstraint)) {
+				widthConstraint = 0;
+				foreach (var item in _root)
+					widthConstraint = Math.Max(widthConstraint, item.FrameX + item.FrameWidth);
+			}
+			if (double.IsPositiveInfinity(heightConstraint)) {
+				heightConstraint = 0;
+				foreach (var item in _root)
+					heightConstraint = Math.Max(heightConstraint, item.FrameY + item.FrameHeight);
+			}
+
+			//3. reset Shrink
+			foreach (var child in Children)
+				GetFlexItem(child).Shrink = (float)child.GetValue(ShrinkProperty);
+
+			return new SizeRequest(new Size(widthConstraint, heightConstraint));
+		}
+
+		void Layout(double x, double y, double width, double height)
+		{
+			if (_root.Parent != null)	//Layout is only computed at root level
+				return;
+			_root.Left = (float)x;
+			_root.Top = (float)y;
+			_root.Width = !double.IsPositiveInfinity((width)) ? (float)width : 0;
+			_root.Height = !double.IsPositiveInfinity((height)) ? (float)height : 0;
+			_root.Layout();
+		}
+	}
+
+	static class FlexExtensions
+	{
+		public static int IndexOf(this Flex.Item parent, Flex.Item child)
+		{
+			var index = -1;
+			foreach (var it in parent) {
+				index++;
+				if (it == child)
+					return index;
+			}
+			return -1;
+		}
+
+		public static void Remove(this Flex.Item parent, Flex.Item child)
+		{
+			var index = parent.IndexOf(child);
+			if (index < 0)
+				return;
+			parent.RemoveAt(index);
+		}
+
+		public static Rectangle GetFrame(this Flex.Item item)
+		{
+			return new Rectangle(item.FrameX, item.FrameY, item.FrameWidth, item.FrameHeight);
+		}
+
+		public static (double widthConstraint, double heightConstraint) GetConstraints(this Flex.Item item)
+		{
+			var widthConstraint = -1d;
+			var heightConstraint = -1d;
+
+			var parent = item.Parent;
+			do {
+				if (parent == null)
+					break;
+				if (widthConstraint < 0 && !float.IsNaN(parent.Width))
+					widthConstraint = (double)parent.Width;
+				if (heightConstraint < 0 && !float.IsNaN(parent.Height))
+					heightConstraint = (double)parent.Height;
+				parent = parent.Parent;
+			} while (widthConstraint < 0 || heightConstraint < 0);
+			return (widthConstraint, heightConstraint);
+		}
+
+		public static float ToFlexFloat(this FlexBasis basis)
+		{
+			if (basis.IsAuto)
+				return float.NaN;
+			return basis.Length;
+		}
+	}
+}
