39990a2488c722240ff21e8078523ecd8df9ed41
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-03-12 21:50:19 +0100

    Updated CornerRadius class (added Equals, operator ==, operator !=, GetHashCode and Deconstruct) Added CornerRadiusTypeConverter

--- Xamarin.Forms.Core/CornerRadius.cs
+++ Xamarin.Forms.Core/CornerRadius.cs
@@ -1,38 +1,78 @@
-﻿using System;
+﻿using System.Diagnostics;
 
 namespace Xamarin.Forms.Core
 {
+	[DebuggerDisplay ("TopLeft={TopLeft}, TopRight={TopRight}, BottomLeft={BottomLeft}, BottomRight={BottomRight}")]
+	[TypeConverter (typeof (CornerRadiusTypeConverter))]
 	public struct CornerRadius
 	{
-		public double TopLeft { get; set; }
-		public double TopRight { get; set; }
-		public double BottomLeft { get; set; }
-		public double BottomRight { get; set; }
+		public double TopLeft { get; private set; }
+		public double TopRight { get; private set; }
+		public double BottomLeft { get; private set; }
+		public double BottomRight { get; private set; }
 
-		public CornerRadius(double uniformRadius) : this(uniformRadius, uniformRadius, uniformRadius, uniformRadius)
+		internal bool IsDefault
+		{
+			get { return TopLeft == 0 && TopRight == 0 && BottomLeft == 0 && BottomRight == 0; }
+		}
+
+		public CornerRadius (double uniformRadius) : this (uniformRadius, uniformRadius, uniformRadius, uniformRadius)
 		{
 		}
 
-		public CornerRadius(double topLeft, double topRight, double bottomLeft, double bottomRight)
+		public CornerRadius (double topLeft, double topRight, double bottomLeft, double bottomRight)
 		{
-			if (topLeft < 0 || double.IsNaN(topLeft))
-				throw new ArgumentException("TopLeft is less than 0 or is not of type " + typeof(double), nameof(topLeft));
-			if (topRight < 0 || double.IsNaN(topRight))
-				throw new ArgumentException("TopRight is less than 0 or is not of type " + typeof(double), nameof(topRight));
-			if (bottomLeft < 0 || double.IsNaN(bottomLeft))
-				throw new ArgumentException("BottomLeft is less than 0 or is not of type " + typeof(double), nameof(bottomLeft));
-			if (bottomRight < 0 || double.IsNaN(bottomRight))
-				throw new ArgumentException("BottomRight is less than 0 or is not of type " + typeof(double), nameof(bottomRight));
-
 			TopLeft = topLeft;
 			TopRight = topRight;
 			BottomLeft = bottomLeft;
 			BottomRight = bottomRight;
 		}
 
-		public static implicit operator CornerRadius(double uniformRadius)
+		public static implicit operator CornerRadius (double uniformRadius)
 		{
-			return new CornerRadius(uniformRadius);
+			return new CornerRadius (uniformRadius);
+		}
+
+		bool Equals (CornerRadius other)
+		{
+			return TopLeft.Equals (other.TopLeft) && TopRight.Equals (other.TopRight) && BottomLeft.Equals (other.BottomLeft) && BottomRight.Equals (other.BottomRight);
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (ReferenceEquals (null, obj))
+				return false;
+
+			return obj is CornerRadius && Equals ((CornerRadius)obj);
+		}
+
+		public override int GetHashCode ()
+		{
+			unchecked {
+				int hashCode = TopLeft.GetHashCode();
+				hashCode = (hashCode * 397) ^ TopRight.GetHashCode ();
+				hashCode = (hashCode * 397) ^ BottomLeft.GetHashCode ();
+				hashCode = (hashCode * 397) ^ BottomRight.GetHashCode ();
+				return hashCode;
+			}
+		}
+
+		public static bool operator == (CornerRadius left, CornerRadius right)
+		{
+			return left.Equals (right);
+		}
+
+		public static bool operator != (CornerRadius left, CornerRadius right)
+		{
+			return !left.Equals (right);
+		}
+
+		public void Deconstruct (out double topLeft, out double topRight, out double bottomLeft, out double bottomRight)
+		{
+			topLeft = TopLeft;
+			topRight = TopRight;
+			bottomLeft = BottomLeft;
+			bottomRight = BottomRight;
 		}
 	}
 }
---
+++ Xamarin.Forms.Core/CornerRadiusTypeConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Xamarin.Forms.Core
+{
+	[Xaml.ProvideCompiled ("Xamarin.Forms.Core.XamlC.CornerRadiusTypeConverter")]
+	[Xaml.TypeConversion (typeof (CornerRadius))]
+	public class CornerRadiusTypeConverter : TypeConverter
+	{
+		public override object ConvertFromInvariantString (string value)
+		{
+			if (value != null) {
+				value = value.Trim ();
+				if (value.Contains (",")) { //Xaml
+					var cornerRadius = value.Split(',');
+					switch (cornerRadius.Length) {
+						case 4:
+							if (double.TryParse (cornerRadius[0], NumberStyles.Number, CultureInfo.InvariantCulture, out double topLeft)
+								&& double.TryParse (cornerRadius[1], NumberStyles.Number, CultureInfo.InvariantCulture, out double topRight)
+								&& double.TryParse (cornerRadius[2], NumberStyles.Number, CultureInfo.InvariantCulture, out double bottomLeft)
+								&& double.TryParse (cornerRadius[3], NumberStyles.Number, CultureInfo.InvariantCulture, out double bottomRight))
+								return new CornerRadius (topLeft, topRight, bottomLeft, bottomRight);
+							break;
+					}
+				} else if (value.Contains (" ")) { //CSS
+					var cornerRadius = value.Split(' ');
+					switch (cornerRadius.Length) {
+						case 4:
+							if (double.TryParse (cornerRadius[0], NumberStyles.Number, CultureInfo.InvariantCulture, out double topLeft)
+								&& double.TryParse (cornerRadius[1], NumberStyles.Number, CultureInfo.InvariantCulture, out double topRight)
+								&& double.TryParse (cornerRadius[2], NumberStyles.Number, CultureInfo.InvariantCulture, out double bottomLeft)
+								&& double.TryParse (cornerRadius[3], NumberStyles.Number, CultureInfo.InvariantCulture, out double bottomRight))
+								return new CornerRadius (topLeft, topRight, bottomLeft, bottomRight);
+							break;
+					}
+				} else { //single uniform CornerRadius
+					if (double.TryParse (value, NumberStyles.Number, CultureInfo.InvariantCulture, out double l))
+						return new CornerRadius (l);
+				}
+			}
+
+			throw new InvalidOperationException ($"Cannot convert \"{value}\" into {typeof (CornerRadius)}");
+		}
+	}
+}
