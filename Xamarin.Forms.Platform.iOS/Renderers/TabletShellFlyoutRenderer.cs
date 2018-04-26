59c12bfde8d519b24c2080d26f3743f1a10a755b
Jason Smith <jas@microsoft.com>
2018-04-26 02:01:02 -0700

    Use UISplitViewController on Tablet

---
+++ Xamarin.Forms.Platform.iOS/Renderers/TabletShellFlyoutRenderer.cs
@@ -0,0 +1,48 @@
+﻿using UIKit;
+
+namespace Xamarin.Forms.Platform.iOS
+{
+	public class TabletShellFlyoutRenderer : UISplitViewController, IShellFlyoutRenderer
+	{
+		#region IShellFlyoutRenderer
+
+		UIViewController IShellFlyoutRenderer.ViewController => this;
+
+		UIView IShellFlyoutRenderer.View => View;
+
+		void IShellFlyoutRenderer.AttachFlyout(IShellContext context, UIViewController content)
+		{
+			_context = context;
+			_content = content;
+			
+			FlyoutContent = _context.CreateShellFlyoutContentRenderer();
+
+			ViewControllers = new UIViewController[]
+			{
+				FlyoutContent.ViewController,
+				content
+			};
+		}
+
+		void IShellFlyoutRenderer.CloseFlyout()
+		{
+		}
+
+		#endregion
+
+		private IShellContext _context;
+		private UIViewController _content;
+
+		private IShellFlyoutContentRenderer FlyoutContent { get; set; }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+		}
+
+		public override void ViewDidLayoutSubviews()
+		{
+			base.ViewDidLayoutSubviews();
+		}
+	}
+}
