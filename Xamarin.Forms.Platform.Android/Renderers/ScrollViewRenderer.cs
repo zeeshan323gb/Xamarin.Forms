a47f91c0ad14b965f6eb62fea85854a76dbe938b
Jason Smith <jas@microsoft.com>
2018-05-14 22:35:48 -0700

    Fix bug where android scrollview would sometimes not update child layout

--- Xamarin.Forms.Platform.Android/Renderers/ScrollViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/ScrollViewRenderer.cs
@@ -220,6 +220,8 @@ namespace Xamarin.Forms.Platform.Android
 			bool requestContainerLayout = bottom > _previousBottom;
 			_previousBottom = bottom;
 
+			_container.Measure(MeasureSpecFactory.MakeMeasureSpec(right - left, MeasureSpecMode.Unspecified),
+				MeasureSpecFactory.MakeMeasureSpec(bottom - top, MeasureSpecMode.Unspecified));
 			base.OnLayout(changed, left, top, right, bottom);
 			if (_view.Content != null && _hScrollView != null)
 				_hScrollView.Layout(0, 0, right - left, Math.Max(bottom - top, (int)Context.ToPixels(_view.Content.Height)));
