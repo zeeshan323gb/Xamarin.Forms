79ba395e635d7b40ae6b1f5c94d52735e5f609c6
Jason Smith <jas@microsoft.com>
2018-05-17 00:01:19 -0700

    [iOS Shell] Support SearchHandler.SearchBoxVisibility dynamic updates

--- Xamarin.Forms.Platform.iOS/Renderers/IShellTabItemRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/IShellTabItemRenderer.cs
@@ -5,9 +5,9 @@ namespace Xamarin.Forms.Platform.iOS
 {
 	public interface IShellTabItemRenderer : IDisposable
 	{
+		bool IsInMoreTab { get; set; }
 		Page Page { get; }
 		ShellTabItem ShellTabItem { get; set; }
 		UIViewController ViewController { get; }
-		bool IsInMoreTab { get; set; }
 	}
 }
