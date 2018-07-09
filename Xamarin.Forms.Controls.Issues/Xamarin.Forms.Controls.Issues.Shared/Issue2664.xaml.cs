5869fad6fdfb6e759926286c65685a1343d79ed7
Samantha Houts <samantha.houts@xamarin.com>
2018-07-09 15:48:03 -0700

    Fix test case

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2664.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2664.xaml.cs
@@ -5,10 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace Xamarin.Forms.Controls.Issues
 {
+#if APP
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 2664, "", PlatformAffected.Android)]
 	[XamlCompilation(XamlCompilationOptions.Skip)]
 	public partial class Issue2664 : ContentPage
 	{
@@ -17,4 +22,5 @@ namespace Xamarin.Forms.Controls.Issues
 			InitializeComponent ();
 		}
 	}
+#endif
 }
