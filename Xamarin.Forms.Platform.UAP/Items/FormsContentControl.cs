76d1db2ea856dfee975a0f948f40861a1d8a2eb6
E.Z. Hart <hartez@gmail.com>
2018-07-25 09:07:07 -0600

    Adding some notes for FormsContentControl

--- Xamarin.Forms.Platform.UAP/Items/FormsContentControl.cs
+++ Xamarin.Forms.Platform.UAP/Items/FormsContentControl.cs
@@ -7,6 +7,7 @@ using Xamarin.Forms.Internals;
 namespace Xamarin.Forms.Platform.UWP
 {
 	// TODO hartez 2018/06/28 09:08:55 This should really be named something like FormsItemControl/FormsItemsContentControl? Not sure yet.
+	// ItemContentControl right now on Android
 	public class FormsContentControl : ContentControl
 	{
 		public FormsContentControl()
@@ -35,6 +36,7 @@ namespace Xamarin.Forms.Platform.UWP
 			set => SetValue(FormsDataTemplateProperty, value);
 		}
 
+		// TODO hartez 2018/07/24 11:02:39 Use nameof	
 		public static readonly DependencyProperty FormsDataContextProperty = DependencyProperty.Register(
 			"FormsDataContext", typeof(object), typeof(FormsContentControl), 
 			new PropertyMetadata(default(object), FormsDataContextChanged));
@@ -66,6 +68,9 @@ namespace Xamarin.Forms.Platform.UWP
 
 				_rootElement = visualElement;
 				_rootElement.MeasureInvalidated += RootElementOnMeasureInvalidated;
+
+				// TODO hartez 2018/07/24 11:22:05 Using GetOrCreate might be a waste here, since we're creating the element in this method	
+				// The "Get" part of GetOrCreate is checking a value that's certain to not be set. Change this to just create the renderer
 				Content = visualElement.GetOrCreateRenderer().ContainerElement;
 			}
 
@@ -100,6 +105,7 @@ namespace Xamarin.Forms.Platform.UWP
 			Size request = _rootElement.Measure(availableSize.Width, availableSize.Height, 
 				MeasureFlags.IncludeMargins).Request;
 
+			// TODO hartez 2018/07/24 11:01:41 With GetNativeSize working, I don't think this next set of ifs is entirely necessary 
 			if (request.Width < 0)
 			{
 				request.Width = 100;
@@ -110,6 +116,7 @@ namespace Xamarin.Forms.Platform.UWP
 				request.Height = 100;
 			}
 
+			// TODO hartez 2018/07/25 08:51:52 To make this look nicer, how about a LayoutOrigin extension method on VisualElement that just takes a size	
 			_rootElement.Layout(new Rectangle(0, 0, request.Width, request.Height));
 
 			return new Windows.Foundation.Size(request.Width, request.Height); 
