bdcd02ec18e3b475c3d867c05d50240082282787
E.Z. Hart <hartez@gmail.com>
2018-07-25 11:54:02 -0600

    Clean up TemplateCodeCollectionViewGallery

---
+++ Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/TemplateCodeCollectionViewGallery.cs
@@ -0,0 +1,47 @@
+﻿namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries
+{
+	internal class TemplateCodeCollectionViewGallery : ContentPage
+	{
+		public TemplateCodeCollectionViewGallery(IItemsLayout itemsLayout)
+		{
+			var layout = new Grid
+			{
+				RowDefinitions = new RowDefinitionCollection
+				{
+					new RowDefinition { Height = GridLength.Auto },
+					new RowDefinition { Height = GridLength.Star }
+				}
+			};
+
+			var itemTemplate = new DataTemplate(() =>
+			{
+				var view = new Image
+				{
+					Source = "oasis.jpg",
+					
+					WidthRequest = 50,
+					HeightRequest = 50
+				};
+
+				return view;
+			});
+
+			var collectionView = new CollectionView
+			{
+				ItemsLayout = itemsLayout,
+				ItemTemplate = itemTemplate
+			};
+
+			var generator = new ItemsSourceGenerator(collectionView, initialItems: 2);
+
+			layout.Children.Add(generator);
+			layout.Children.Add(collectionView);
+
+			Grid.SetRow(collectionView, 1);
+
+			Content = layout;
+
+			generator.GenerateItems();
+		}
+	}
+}
