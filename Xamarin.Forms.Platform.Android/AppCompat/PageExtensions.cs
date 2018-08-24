e7476f55ee8fc954964091b2c0ead8dbfb8cfcb7
Rui Marinho <me@ruimarinho.net>
2018-08-24 20:59:52 +0100

    [Android] Remove app fragment usage

--- Xamarin.Forms.Platform.Android/AppCompat/PageExtensions.cs
+++ Xamarin.Forms.Platform.Android/AppCompat/PageExtensions.cs
@@ -8,24 +8,25 @@ namespace Xamarin.Forms.Platform.Android
 {
 	public static class PageExtensions
 	{
-		public static Fragment CreateFragment(this ContentPage view, Context context)
-		{
-			if (!Forms.IsInitialized)
-				throw new InvalidOperationException("call Forms.Init() before this");
+		//public static Fragment CreateFragment(this ContentPage view, Context context)
+		//{
 
-			if (!(view.RealParent is Application))
-			{
-				Application app = new DefaultApplication();
-				app.MainPage = view;
-			}
+		//	if (!Forms.IsInitialized)
+		//		throw new InvalidOperationException("call Forms.Init() before this");
 
-			var platform = new Platform(context, true);
-			platform.SetPage(view);
+		//	if (!(view.RealParent is Application))
+		//	{
+		//		Application app = new DefaultApplication();
+		//		app.MainPage = view;
+		//	}
 
-			var vg = platform.GetViewGroup();
+		//	var platform = new Platform(context, true);
+		//	platform.SetPage(view);
 
-			return new EmbeddedFragment(vg, platform);
-		}
+		//	var vg = platform.GetViewGroup();
+
+		//	return new EmbeddedFragment(vg, platform);
+		//}
 
 		public static global::Android.Support.V4.App.Fragment CreateSupportFragment(this ContentPage view, Context context)
 		{
@@ -50,45 +51,45 @@ namespace Xamarin.Forms.Platform.Android
 		{
 		}
 
-		class EmbeddedFragment : Fragment
-		{
-			readonly ViewGroup _content;
-			readonly Platform _platform;
-			bool _disposed;
+		//class EmbeddedFragment :  Android.Support.V4.App.Fragment
+		//{
+		//	readonly ViewGroup _content;
+		//	readonly Platform _platform;
+		//	bool _disposed;
 
-			// ReSharper disable once UnusedMember.Local (Android uses this on configuration change
-			public EmbeddedFragment()
-			{
-			}
+		//	// ReSharper disable once UnusedMember.Local (Android uses this on configuration change
+		//	public EmbeddedFragment()
+		//	{
+		//	}
 
-			public EmbeddedFragment(ViewGroup content, Platform platform)
-			{
-				_content = content;
-				_platform = platform;
-			}
+		//	public EmbeddedFragment(ViewGroup content, Platform platform)
+		//	{
+		//		_content = content;
+		//		_platform = platform;
+		//	}
 
-			public override global::Android.Views.View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-			{
-				return _content;
-			}
+		//	public override global::Android.Views.View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		//	{
+		//		return _content;
+		//	}
 
-			protected override void Dispose(bool disposing)
-			{
-				if (_disposed)
-				{
-					return;
-				}
+		//	protected override void Dispose(bool disposing)
+		//	{
+		//		if (_disposed)
+		//		{
+		//			return;
+		//		}
 
-				_disposed = true;
+		//		_disposed = true;
 
-				if (disposing)
-				{
-					(_platform as IDisposable)?.Dispose();
-				}
+		//		if (disposing)
+		//		{
+		//			(_platform as IDisposable)?.Dispose();
+		//		}
 
-				base.Dispose(disposing);
-			}
-		}
+		//		base.Dispose(disposing);
+		//	}
+		//}
 
 		class EmbeddedSupportFragment : global::Android.Support.V4.App.Fragment
 		{
