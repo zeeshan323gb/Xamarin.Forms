2afd7ac6616337e4fe803d392b986d9211f8a355
Rui Marinho <me@ruimarinho.net>
2018-08-27 16:16:48 +0100

    [Android] Obsolete message for ContentPage.CreateFragment()

--- Xamarin.Forms.Platform.Android/AppCompat/PageExtensions.cs
+++ Xamarin.Forms.Platform.Android/AppCompat/PageExtensions.cs
@@ -8,25 +8,67 @@ namespace Xamarin.Forms.Platform.Android
 {
 	public static class PageExtensions
 	{
-		//public static Fragment CreateFragment(this ContentPage view, Context context)
-		//{
+#pragma warning disable 618
+		[Obsolete("ContentPage.CreateFragment() is obsolete as of version 3.2. Please use ContentPage.CreateSupportFragment() instead.")]
+		public static Fragment CreateFragment(this ContentPage view, Context context)
+		{
+			if (!Forms.IsInitialized)
+				throw new InvalidOperationException("call Forms.Init() before this");
 
-		//	if (!Forms.IsInitialized)
-		//		throw new InvalidOperationException("call Forms.Init() before this");
+			if (!(view.RealParent is Application))
+			{
+				Application app = new DefaultApplication();
+				app.MainPage = view;
+			}
 
-		//	if (!(view.RealParent is Application))
-		//	{
-		//		Application app = new DefaultApplication();
-		//		app.MainPage = view;
-		//	}
+			var platform = new Platform(context, true);
+			platform.SetPage(view);
 
-		//	var platform = new Platform(context, true);
-		//	platform.SetPage(view);
+			var vg = platform.GetViewGroup();
 
-		//	var vg = platform.GetViewGroup();
+			return new EmbeddedFragment(vg, platform);
+		}
 
-		//	return new EmbeddedFragment(vg, platform);
-		//}
+		class EmbeddedFragment : Fragment
+		{
+			readonly ViewGroup _content;
+			readonly Platform _platform;
+			bool _disposed;
+
+			// ReSharper disable once UnusedMember.Local (Android uses this on configuration change
+			public EmbeddedFragment()
+			{
+			}
+
+			public EmbeddedFragment(ViewGroup content, Platform platform)
+			{
+				_content = content;
+				_platform = platform;
+			}
+
+			public override global::Android.Views.View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+			{
+				return _content;
+			}
+
+			protected override void Dispose(bool disposing)
+			{
+				if (_disposed)
+				{
+					return;
+				}
+
+				_disposed = true;
+
+				if (disposing)
+				{
+					(_platform as IDisposable)?.Dispose();
+				}
+
+				base.Dispose(disposing);
+			}
+		}
+#pragma warning restore 618
 
 		public static global::Android.Support.V4.App.Fragment CreateSupportFragment(this ContentPage view, Context context)
 		{
@@ -50,46 +92,6 @@ namespace Xamarin.Forms.Platform.Android
 		class DefaultApplication : Application
 		{
 		}
-
-		//class EmbeddedFragment :  Android.Support.V4.App.Fragment
-		//{
-		//	readonly ViewGroup _content;
-		//	readonly Platform _platform;
-		//	bool _disposed;
-
-		//	// ReSharper disable once UnusedMember.Local (Android uses this on configuration change
-		//	public EmbeddedFragment()
-		//	{
-		//	}
-
-		//	public EmbeddedFragment(ViewGroup content, Platform platform)
-		//	{
-		//		_content = content;
-		//		_platform = platform;
-		//	}
-
-		//	public override global::Android.Views.View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-		//	{
-		//		return _content;
-		//	}
-
-		//	protected override void Dispose(bool disposing)
-		//	{
-		//		if (_disposed)
-		//		{
-		//			return;
-		//		}
-
-		//		_disposed = true;
-
-		//		if (disposing)
-		//		{
-		//			(_platform as IDisposable)?.Dispose();
-		//		}
-
-		//		base.Dispose(disposing);
-		//	}
-		//}
 
 		class EmbeddedSupportFragment : global::Android.Support.V4.App.Fragment
 		{
