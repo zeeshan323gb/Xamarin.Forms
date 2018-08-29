1b98e8cd9407aa2755260a15a38308d013c5ae42
E.Z. Hart <hartez@gmail.com>
2018-08-29 09:33:39 -0600

    Move ScrollToRequestEventArgs to its own file

--- Xamarin.Forms.Core/Items/ItemsView.cs
+++ Xamarin.Forms.Core/Items/ItemsView.cs
@@ -28,42 +28,6 @@ namespace Xamarin.Forms
 		}
 	}
 
-	public class ScrollToRequestEventArgs : EventArgs
-	{
-		public ScrollToMode Mode { get; }
-
-		public ScrollToPosition ScrollToPosition { get; }
-		public bool Animate { get; }
-
-		public int Index { get; }
-		public int GroupIndex { get; }
-
-		public object Item { get; }
-		public object Group { get; }
-
-		public ScrollToRequestEventArgs(int index, int groupIndex, 
-			ScrollToPosition scrollToPosition, bool animate)
-		{
-			Mode = ScrollToMode.Position;
-
-			Index = index;
-			GroupIndex = groupIndex;
-			ScrollToPosition = scrollToPosition;
-			Animate = animate;
-		}
-
-		public ScrollToRequestEventArgs(object item, object group, 
-			ScrollToPosition scrollToPosition, bool animate)
-		{
-			Mode = ScrollToMode.Element;
-
-			Item = item;
-			Group = group;
-			ScrollToPosition = scrollToPosition;
-			Animate = animate;
-		}
-	}
-
 	public class ItemsView : View
 	{
 		// TODO hartez 2018/06/24 11:37:00 Give DisplayMemberPath some thought	
---
+++ Xamarin.Forms.Core/Items/ScrollToRequestEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Xamarin.Forms
+{
+	public class ScrollToRequestEventArgs : EventArgs
+	{
+		public ScrollToMode Mode { get; }
+
+		public ScrollToPosition ScrollToPosition { get; }
+		public bool Animate { get; }
+
+		public int Index { get; }
+		public int GroupIndex { get; }
+
+		public object Item { get; }
+		public object Group { get; }
+
+		public ScrollToRequestEventArgs(int index, int groupIndex, 
+			ScrollToPosition scrollToPosition, bool animate)
+		{
+			Mode = ScrollToMode.Position;
+
+			Index = index;
+			GroupIndex = groupIndex;
+			ScrollToPosition = scrollToPosition;
+			Animate = animate;
+		}
+
+		public ScrollToRequestEventArgs(object item, object group, 
+			ScrollToPosition scrollToPosition, bool animate)
+		{
+			Mode = ScrollToMode.Element;
+
+			Item = item;
+			Group = group;
+			ScrollToPosition = scrollToPosition;
+			Animate = animate;
+		}
+	}
+}
