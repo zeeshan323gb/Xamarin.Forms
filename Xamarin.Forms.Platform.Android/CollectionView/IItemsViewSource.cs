56de3501354c36d40894570033063c532b238d4d
E.Z. Hart <hartez@gmail.com>
2018-08-30 09:22:13 -0600

    Clean up todo comments

--- Xamarin.Forms.Platform.Android/CollectionView/IItemsViewSource.cs
+++ Xamarin.Forms.Platform.Android/CollectionView/IItemsViewSource.cs
@@ -2,9 +2,6 @@ namespace Xamarin.Forms.Platform.Android
 {
 	internal interface IItemsViewSource
 	{
-		// TODO hartez 2018/07/30 15:06:20 Maybe this should be ItemsViewSource?
-		// Depends on where we settle for the name of CollectionViewAdapter (ItemsViewAdapter?)
-
 		int Count { get; }
 		object this[int index] { get; }
 	}
