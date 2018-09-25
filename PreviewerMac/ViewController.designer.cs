c4a047c48f8f282c25a4678fe4d8450ecc46f8b7
James Clancey <james.clancey@gmail.com>
2018-09-25 14:20:44 -0800

    Added required designer file

---
+++ PreviewerMac/ViewController.designer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppKit;
+using CoreGraphics;
+using Foundation;
+
+namespace PreviewerMac
+{
+	[Register("ViewController")]
+	partial class ViewController
+	{
+		void ReleaseDesignerOutlets()
+		{
+		}
+	}
+}
